5a92d7a8b5184684195d075e74a0f65e0c839bdc
Patrick Bialek <patrykkrzysztofbialek@gmail.com>
2019-04-20 23:23:52 +0200

    Firebase has been added

---
+++ src/firebase/init.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase';
+import firestore from 'firebase/firestore';
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyBAb1HE9bWcNKuiGnAh8XHXO_0VLNKmTP8",
+  authDomain: "drink-creator-e2966.firebaseapp.com",
+  databaseURL: "https://drink-creator-e2966.firebaseio.com",
+  projectId: "drink-creator-e2966",
+  storageBucket: "drink-creator-e2966.appspot.com",
+  messagingSenderId: "970468632547"
+};
+
+const firebaseApp = firebase.initializeApp(config);
+firebaseApp.firestore().settings({
+  timestampsInSnapshots: true
+});
+
+// Export firebase databse
+export default firebaseApp.firestore()
