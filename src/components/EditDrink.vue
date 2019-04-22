5c524197b26b9850ab29931bc80e3dbf2d6c328e
Patrick Bialek <patrykkrzysztofbialek@gmail.com>
2019-04-23 00:10:51 +0200

    Working on edit drink page

--- src/components/EditDrink.vue
+++ src/components/EditDrink.vue
@@ -1,16 +1,32 @@
 <template>
   <div class="add-drink">
-    <div class="add-drink-content">
+    <div v-if="drink" class="add-drink-content">
       <h2>Edit drink {{ this.$route.params.drink_slug}}</h2>
     </div>
   </div>
 </template>
 
 <script>
+import db from "@/firebase/init";
+
 export default {
   name: "EditDrink",
   data() {
-    return {};
+    return {
+      drink: null
+    };
+  },
+  created() {
+    let ref = db
+      .collection("drinks")
+      .where("slug", "==", this.$route.params.drink_slug);
+
+    ref.get().then(snapshot => {
+      snapshot.forEach(doc => {
+        this.drink = doc.data();
+        this.drink.id = doc.id;
+      });
+    });
   }
 };
 </script>
